a330685b3ffc8d8e7246ba5e7eb3fc1e0e252fa2
Kassahun Tigabu <kassahunshebel@gmail.com>
2025-12-10 15:07:06 +0300

    url of each model

--- ethiocoffee/urls.py
+++ ethiocoffee/urls.py
@@ -1,31 +1,24 @@
-"""
-URL configuration for ethiocoffee project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# ethiocoffee/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('coffeeapp.urls')),  # Main website
+    path('products/', include('products.urls')),
+    path('cart/', include('cart.urls')),
+    path('orders/', include('orders.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('api/', include('api.urls')),
+    
+    # Frontend pages
+    path('about/', TemplateView.as_view(template_name='frontend/about.html'), name='about'),
+    path('contact/', TemplateView.as_view(template_name='frontend/contact.html'), name='contact'),
 ]
 
-# Now connect this URL file to the main project.
-# When someone opens your website
-# Django shows the homepage
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('coffeeapp.urls')),
-]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
